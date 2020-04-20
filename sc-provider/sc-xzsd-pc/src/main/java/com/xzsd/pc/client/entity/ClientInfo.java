26fc35e7c48ea35c1c112b262b857ffc522ec04e
LoveYou8023 <492488086@qq.com>
2020-04-20 10:01:02 +0800

    client

---
+++ sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/client/entity/ClientInfo.java
@@ -0,0 +1,104 @@
+package com.xzsd.pc.client.entity;
+
+/**
+ * 客户信息实体类
+ */
+public class ClientInfo {
+    /**
+     * 用户编号
+     */
+    private String userId;
+    /**
+     * 店铺邀请码
+     */
+    private String inviteCode;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public int getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(int isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    /**
+     * 删除标记
+     */
+    private int isDeleted;
+    /**
+     * 创作时间
+     */
+    private String createTime;
+    /**
+     * 创作者
+     */
+    private String createUser;
+    /**
+     * 更新时间
+     */
+    private String updateTime;
+    /**
+     * 更新者
+     */
+    private String updateUser;
+    /**
+     * 版本号
+     */
+    private String version;
+}
